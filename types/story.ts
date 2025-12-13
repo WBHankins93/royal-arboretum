a94291f3e1158799563d1fc6deb2716cb5390a4f
Ben Hankins <WBHankins93@gmail.com>
2025-12-12 18:55:50 -0600

    Fix syntax error: Remove duplicate code in story.ts

--- types/story.ts
+++ types/story.ts
@@ -123,68 +123,6 @@ export const getDesktopStoryPages = (): StoryPage[] => {
 
 // Default to desktop, but will be overridden by mobile detection
 export const storyPages: StoryPage[] = getDesktopStoryPages();
-  {
-    id: 'pathfinder-frog',
-    type: 'character',
-    title: 'The Pathfinder',
-    description: 'A brave explorer who maps the unknown paths of the arboretum',
-    imageSrc: '/images/pathfinder-frog.png',
-    characterName: 'Pathfinder',
-  },
-  {
-    id: 'tophat-frog',
-    type: 'character',
-    title: 'The Gentleman',
-    description: 'A distinguished frog with impeccable style and wisdom',
-    imageSrc: '/images/tophat-frog.png',
-    characterName: 'Gentleman',
-  },
-  {
-    id: 'star-frog',
-    type: 'character',
-    title: 'The Stargazer',
-    description: 'An astronomer who studies the stars from the observatory',
-    imageSrc: '/images/star-frog.png',
-    characterName: 'Stargazer',
-  },
-  {
-    id: 'mushroom-frog',
-    type: 'character',
-    title: 'The Mycologist',
-    description: 'A scholar of fungi and the mysteries of the forest floor',
-    imageSrc: '/images/mushroom-frog.png',
-    characterName: 'Mycologist',
-  },
-  {
-    id: 'fishing-frog',
-    type: 'character',
-    title: 'The Angler',
-    description: 'A patient fisher who tends to the water wheel and pond',
-    imageSrc: '/images/fishing-frog.png',
-    characterName: 'Angler',
-  },
-  {
-    id: 'fireplace-frogs',
-    type: 'character',
-    title: 'The Hearth Keepers',
-    description: 'Guardians of the Great Hall, keeping the fire warm and stories alive',
-    imageSrc: '/images/fireplace-frogs.png',
-    characterName: 'Hearth Keepers',
-  },
-  {
-    id: 'greathall-blueprint',
-    type: 'blueprint',
-    title: 'Great Hall Blueprint',
-    description: 'Architectural plans for the Great Hall',
-    imageSrc: '/images/greathall-blueprint.png',
-  },
-  {
-    id: 'character-gallery',
-    type: 'gallery',
-    title: 'Character Gallery',
-    description: 'Meet all the residents of the Royal Arboretum',
-  },
-];
 
 // Helper function to get all character pages (works with both mobile and desktop)
 export const getCharacterPages = (isMobile: boolean = false): StoryPage[] => {
